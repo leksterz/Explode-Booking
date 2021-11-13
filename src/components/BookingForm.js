ee405d137660b561c012127bfb491f5dad209526
kipk15 <88164334+kipk15@users.noreply.github.com>
2021-11-12 21:56:39 -0500

    Time component

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -4,11 +4,12 @@ import "react-datepicker/dist/react-datepicker.css";
 import { addDays } from "date-fns";
 
 import TimeComponent from "./TimeComponent";
+import moment from "moment";
 
 export default function BookingForm({ children }) {
   //availability data model
   const availability = [{ gianni: { 0: 1 } }, { jc: { 0: 1 } }];
-
+console.log(moment)
   //request data model
 
   // pass engineers into the dropdown
@@ -18,11 +19,12 @@ export default function BookingForm({ children }) {
   // DatePicker is npm installed. Specify params:
   // min-date, max date and onChange = setDate from state
   const [startDate, setStartDate] = useState(new Date());
+  
 
   //time picker useState
   // define min and max date + onChange
   const [value, setValue] = React.useState(new Date("2014-08-18T21:11:54"));
-
+    
   return (
     <div className="container">
       <div className="title">
@@ -40,12 +42,15 @@ export default function BookingForm({ children }) {
         <br />
         <DatePicker
           selected={startDate}
-          minDate={new Date()}
-          maxDate={addDays(new Date(), 10)}
+          minDate={addDays(new Date(), 5)}
+          maxDate={addDays(new Date(), 9)}
           onChange={(date) => setStartDate(date)}
         />
         <br />
-        <TimeComponent />
+        <TimeComponent 
+      
+  
+            />
 
         <br />
         <br />
--- src/components/TimeComponent.js
+++ src/components/TimeComponent.js
@@ -4,28 +4,40 @@ import moment from "moment";
 import TimeRange from "react-time-range";
 
 export default class TimeComponent extends Component {
+  
   state = {
-    startTime: moment().startOf("hour").add(13, "hour"),
-    endTime: moment().startOf("hour").add(14, "hour"),
+    startTime: moment().startOf("hour").add(0, "hour"),
+    endTime: moment().startOf("hour").add(0, "hour"),
   };
-
+  
+  
   returnFunctionStart = (event) => {
     this.setState({ startTime: event.startTime });
   };
 
   returnFunctionEnd = (event) => {
+    
     this.setState({ endTime: event.endTime });
   };
-
   render() {
+    
     return (
+      
       <TimeRange
+
+        //start and end times responsive to user action
         onStartTimeChange={this.returnFunctionStart}
         onEndTimeChange={this.returnFunctionEnd}
+
+        //Displayed start and end times
         startMoment={this.state.startTime}
         endMoment={this.state.endTime}
+        
+        
         minuteIncrement={60}
       />
+
     );
   }
 }
+  
